1686440f8d57fe2e12d22a348e0d9e5fb7b94ab2
Island <islandhuynh@gmail.com>
2021-05-07 18:53:36 -0400

    Element are removed from column when dragged to a new one

--- src/components/project-tracker/ProjectTracker.tsx
+++ src/components/project-tracker/ProjectTracker.tsx
@@ -99,32 +99,6 @@ const selectedProject = {
   complete: [
     "Board",
     "Standard Pieces",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
-    "Create Authentification with Firebase",
-    "Connect to Geo API",
-    "Connect to Places API",
   ],
   onHold: [
     "Intermediate Computer",
@@ -144,6 +118,7 @@ export const ProjectTracker = () => {
   const [newTask, setNewTask] = useState<string | undefined>('');
   const [selectedColumn, setSelectedColumn] = useState<keyof columnList | undefined>(undefined);
   const [draggedTask, setDraggedTask] = useState<string | undefined>(undefined);
+  const [removedItemColumn, setRemovedItemColumn] = useState<keyof columnList | undefined>(undefined);
 
   const createColumn = <C extends keyof columnList>(columnName: C): JSX.Element => {
     let columnTasks: string[] = [];
@@ -169,6 +144,7 @@ export const ProjectTracker = () => {
         onDrop={() => {
           if (draggedTask) {
             addTask(draggedTask, columnName)
+            if (removedItemColumn) removeTask(draggedTask, removedItemColumn)
             setDraggedTask(undefined)
           }
         }}
@@ -177,7 +153,7 @@ export const ProjectTracker = () => {
           <h1>{columnTitles[columnName]}</h1>
         </span>
         <div id={`${columnName}-content`} className="custom-scroll">
-          {createTaskList(columnTasks)}
+          {createTaskList(columnTasks, columnName)}
         </div>
         {newInputVisibility && selectedColumn === columnName ?
           <>
@@ -233,13 +209,40 @@ export const ProjectTracker = () => {
     }
   }
 
-  const createTaskList = (taskArray: string[]) => {
+  const removeTask = <C extends keyof columnList>(task: string, column: C) => {
+    let index = -1;
+
+    switch (column) {
+      case columnSelection.BACKLOG:
+        index = selectedProject.backlog.indexOf(task);
+        if (index > -1) selectedProject.backlog.splice(index, 1);
+        break;
+      case columnSelection.PROGRESS:
+        index = selectedProject.progress.indexOf(task);
+        if (index > -1) selectedProject.progress.splice(index, 1);
+        break;
+      case columnSelection.COMPLETE:
+        index = selectedProject.complete.indexOf(task);
+        if (index > -1) selectedProject.complete.splice(index, 1);
+        break;
+      case columnSelection.ON_HOLD:
+        index = selectedProject.onHold.indexOf(task);
+        if (index > -1) selectedProject.onHold.splice(index, 1);
+        break;
+    }
+  }
+
+
+  const createTaskList = <C extends keyof columnList>(taskArray: string[], column: C) => {
     let listArray: JSX.Element[] = [];
     taskArray.forEach(task => listArray.push(
       <li
         className="task-item"
         draggable={true}
-        onDragStart={() => setDraggedTask(task)}
+        onDragStart={() => {
+          setDraggedTask(task)
+          setRemovedItemColumn(column)
+        }}
       >
         {task}
       </li>
