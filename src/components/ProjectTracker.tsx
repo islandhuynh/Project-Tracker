761602a4d08a785cff7e8df69e347f37d60b12d7
Island <islandhuynh@gmail.com>
2021-05-07 16:58:39 -0400

    Input box shows up when add item button is clicked - initial styling started

--- src/components/ProjectTracker.tsx
+++ src/components/ProjectTracker.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 const mockData = {
   email: 'islandhuynh@gmail.com',
   uuid: '123abc',
@@ -8,7 +9,7 @@ const mockData = {
       completeStatus: false,
       backlog: [
         "Drag and Drop Logic for Pieces",
-        "Rotate Pieces"
+        "Rotate Pieces",
       ],
       progress: [
         "Computer Logic"
@@ -16,6 +17,32 @@ const mockData = {
       complete: [
         "Board",
         "Standard Pieces",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
+        "Create Authentification with Firebase",
+        "Connect to Geo API",
+        "Connect to Places API",
       ],
       onHold: [
         "Intermediate Computer",
@@ -31,7 +58,8 @@ const mockData = {
       progress: [
         "Create Authentification with Firebase",
         "Connect to Geo API",
-        "Connect to Places API"
+        "Connect to Places API",
+        "Create Authentification with Firebase",
       ],
       complete: [
         "Button Container",
@@ -59,11 +87,8 @@ const columnTitles: columnList = {
 }
 
 export const ProjectTracker = () => {
-  const createTaskList = (taskArray: string[]) => {
-    let listArray: JSX.Element[] = [];
-    taskArray.forEach(task => listArray.push(<li className="task-item">{task}</li>));
-    return listArray;
-  }
+  const [newInputVisibility, setNewInputVisibility] = useState(false);
+  const [newTask, setNewTask] = useState<string | undefined>('');
 
   const createColumn = <C extends keyof columnList>(columnName: C): JSX.Element => {
     let columnTasks: string[] = [];
@@ -90,8 +115,33 @@ export const ProjectTracker = () => {
         <div id={`${columnName}-content`} className="custom-scroll">
           {createTaskList(columnTasks)}
         </div>
+        {newInputVisibility ?
+          <div className="new-input-container">
+            <textarea
+              className="new-input"
+              value={newTask} onChange={e => setNewTask(e.target.value)}
+            />
+          </div>
+          :
+          null
+        }
+        <div className="add-btn-group">
+          <div className="add-btn" onClick={() => setNewInputVisibility(true)}>
+            <span className="plus-sign">+</span>
+            <span>Add Item</span>
+          </div>
+          <div className="add-btn solid" onClick={() => setNewInputVisibility(false)}>
+            <span>Save Item</span>
+          </div>
+        </div>
       </li>
     )
+  }
+
+  const createTaskList = (taskArray: string[]) => {
+    let listArray: JSX.Element[] = [];
+    taskArray.forEach(task => listArray.push(<li className="task-item">{task}</li>));
+    return listArray;
   }
 
   return (
